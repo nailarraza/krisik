e29b80f8e5f093c9726eb364e1315b24c09e7c78
nailarraza <nailarraza16@gmail.com>
2024-09-11 13:15:07 +0700

    update:  API untuk Demografis

--- pages/api/wilayah.js
+++ pages/api/wilayah.js
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const wilayah = [
-    { "id": 1, "name": "Dusun A", "rt": "3", "kk": "10", "male": "500", "female": "800",  "total": "1300" }, 
-    { "id": 2, "name": "Dusun B", "rt": "5", "kk": "20", "male": "600", "female": "900",  "total": "1500" },
-    { "id": 3, "name": "Dusun C", "rt": "7", "kk": "30", "male": "700", "female": "1000", "total": "1700" },
+    { "id": 1, "name": "Krisik", "rt": "0", "kk": "0", "male": "0", "female": "0",  "total": "0" }, 
+    { "id": 2, "name": "Reco", "rt": "", "kk": "0", "male": "0", "female": "0",  "total": "0" },
+    { "id": 3, "name": "Surokoyo", "rt": "", "kk": "0", "male": "0", "female": "0", "total": "0" },
 ];
 
 export default function allHandler(req, res) {
