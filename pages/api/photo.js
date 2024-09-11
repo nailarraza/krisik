c23cf44705743aa320908e2caf774ba409448b5a
nailarraza <nailarraza16@gmail.com>
2024-09-11 11:18:32 +0700

    update:  API dari Photo, Video

--- pages/api/photo.js
+++ pages/api/photo.js
@@ -34,25 +34,25 @@ const photos = [
     src: "https://i.pinimg.com/originals/5d/32/8d/5d328decb393bddb9bf2243296f892b2.jpg",
     title: "Image 6",
     width: 4,
-    height: 3,
+    height: 4,
   },
   {
     src: "https://i.pinimg.com/564x/2e/3e/75/2e3e75d65aaab97dd7f6fd123f2b7afe.jpg",
     title: "Image 7",
     width: 4,
-    height: 3,
+    height: 4,
   },
   {
     src: "https://i.pinimg.com/564x/8b/b6/a2/8bb6a2acaff2b6253767dbd3c017b11b.jpg",
     title: "Image 8",
     width: 4,
-    height: 3,
+    height: 4,
   },
   {
     src: "https://i.pinimg.com/564x/4f/56/af/4f56af12d41c56ee2bcfe8ab09fff2b5.jpg",
     title: "Image 9",
     width: 4,
-    height: 3,
+    height: 4,
   },
 ];
 
