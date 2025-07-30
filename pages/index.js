8ba6de519c22c314c287b12c785cde1132661638
fikrisc <fahrifikri@apps.ipb.ac.id>
2025-07-30 04:23:16 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -284,7 +284,7 @@ export default function Home({ posts, agendas, videos, photos }) {
             <Swiper
               className="swiper-custom"
               spaceBetween={24}
-              slidesPerView={3}
+              slidesPerView={6}
               breakpoints={{
                 320: {
                   slidesPerView: 1,
